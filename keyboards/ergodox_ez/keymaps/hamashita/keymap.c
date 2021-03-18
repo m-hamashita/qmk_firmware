2c5803c4f0f3e628ca21ebb199c14fa77ae94190
m.hama <masakatsu.hamashita@gunosy.com>
2021-03-18 21:45:28 +0900

    Update keymap

--- keyboards/ergodox_ez/keymaps/hamashita/keymap.c
+++ keyboards/ergodox_ez/keymaps/hamashita/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |  LCtl  |A/LCtl|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;/RCtl| Enter  |
  * |--------+------+------+------+------+------|  End |           | PgDn |------+------+------+------+------+--------|
- * |  LSft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   '    |
+ * |  LSft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RSft   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      | LAlt |      | LGUI |                                       | RGUI |~LCode|~LNav |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,        KC_6,       KC_7,             KC_8,       KC_9,       KC_0,             KC_MINS,
         KC_PGUP,        KC_Y,       KC_U,             KC_I,       KC_O,       KC_P,             KC_BSLS,
                         KC_H,       KC_J,             KC_K,       KC_L,       RCTL_T(KC_SCLN),  KC_ENT,
-        KC_PGDN,        KC_N,       KC_M,             KC_COMM,    KC_DOT,     KC_SLSH,          KC_QUOT,
+        KC_PGDN,        KC_N,       KC_M,             KC_COMM,    KC_DOT,     KC_SLSH,          KC_RSFT,
                                     KC_RGUI,          MO(L_CODE), MO(L_NAV),  XXXXXXX,          XXXXXXX,
         XXXXXXX,        XXXXXXX,
         XXXXXXX,
