eec0cffb117ab8a2ca52ea41e96683fbddf6c0c0
m.hama <masakatsu.hamashita@gunosy.com>
2021-06-01 13:44:16 +0900

    Update keymap

--- keyboards/ergodox_ez/keymaps/hamashita/keymap.c
+++ keyboards/ergodox_ez/keymaps/hamashita/keymap.c
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_ESC,         KC_1,       KC_2,       KC_3,                KC_4,              KC_5,               XXXXXXX,
-        KC_TAB,         KC_Q,       KC_W,       KC_E,                KC_R,              KC_T,               KC_HOME,
+        KC_TAB,         KC_Q,       KC_W,       KC_E,                KC_R,              KC_T,               XXXXXXX,
         KC_LCTL,        KC_A,       KC_S,       KC_D,                KC_F,              KC_G,
-        KC_LSFT,        KC_Z,       KC_X,       KC_C,                KC_V,              KC_B,               KC_END,
+        KC_LSFT,        KC_Z,       KC_X,       KC_C,                KC_V,              KC_B,               XXXXXXX,
         XXXXXXX,        XXXXXXX,    KC_LALT,    MO(L_CODE),           KC_LGUI,
                                                                                         XXXXXXX,            XXXXXXX,
                                                                                                             XXXXXXX,
@@ -149,17 +149,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_NAV] = LAYOUT_ergodox(
        // left hand
        KC_GRV,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F11,
-       _______, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
+       _______, _______,    _______,    _______,    _______,    _______,    _______,
        _______, KC_1,       KC_2,       KC_3,       KC_4,       KC_5,
-       _______, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
-       XXXXXXX, XXXXXXX,    XXXXXXX,    _______,    _______,
+       _______, _______,    _______,    _______,    _______,    _______,    _______,
+       _______, _______,    _______,    _______,    _______,
                                                                 _______,    _______,
                                                                             _______,
                                                     _______,    _______,    _______,
        // right hand
        KC_F12,                KC_F6,                 KC_F7,               KC_F8,                  KC_F9,              KC_F10,     KC_DEL,
-       XXXXXXX,               KC_BRIGHTNESS_DOWN,    KC_BRIGHTNESS_UP,    KC_AUDIO_VOL_DOWN,      KC_AUDIO_VOL_UP,    XXXXXXX,    XXXXXXX,
-                              KC_6,                  KC_7,                KC_8,                   KC_9,               KC_0,       XXXXXXX,
+       XXXXXXX,               KC_BRIGHTNESS_DOWN,    KC_BRIGHTNESS_UP,    KC_AUDIO_VOL_DOWN,      KC_AUDIO_VOL_UP,    _______,    _______,
+                              KC_6,                  KC_7,                KC_8,                   KC_9,               KC_0,       _______,
        _______,               _______,               _______,             _______,                _______,            _______,    KC_RSFT,
                                                      _______,             _______,                _______,            _______,    _______,
        _______, _______,
